5e8fe05e70526fb63b7a7f17e1e83f8f8072452e
NhoetKyaiteKaungLay <ohmyqueenmedusa25s@gmail.com>
2024-06-04 08:24:48 +0630

    Update id.py
    Signed-off-by: NhoetKyaiteKaungLay <ohmyqueenmedusa25s@gmail.com>

--- AnonXMusic/plugins/tools/id.py
+++ AnonXMusic/plugins/tools/id.py
@@ -1,4 +1,4 @@
-from BrandrdXMusic import app
+from AnonXMusic import app
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -6,14 +6,14 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def ids(_, message):
     reply = message.reply_to_message
     if reply:
-        button = InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")
+        button = InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="close")
         markup = InlineKeyboardMarkup([[button]])
         message.reply_text(
             f"User {reply.from_user.first_name} ID is : {reply.from_user.id}",
             reply_markup=markup
         )
     else:
-        button = InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")
+        button = InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="close")
         markup = InlineKeyboardMarkup([[button]])
         message.reply(
            f"ᴛʜɪs ɢʀᴏᴜᴩ's ɪᴅ ɪs: {message.chat.id}",
