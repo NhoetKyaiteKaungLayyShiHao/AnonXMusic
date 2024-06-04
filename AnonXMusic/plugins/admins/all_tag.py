554dbaa7e9111082a958cdb3b196accea218d603
NhoetKyaiteKaungLayyShiHao <ohmyqueenmedusa15s@gmail.com>
2024-06-04 18:08:30 +0630

    Update all_tag.py
    Signed-off-by: NhoetKyaiteKaungLayyShiHao <ohmyqueenmedusa15s@gmail.com>

--- AnonXMusic/plugins/admins/all_tag.py
+++ AnonXMusic/plugins/admins/all_tag.py
@@ -66,62 +66,6 @@ async def tag_all_users(_, message):
             pass
 
 @app.on_message(
-    filters.command2(["admin", "mentionadmin"], prefixes=["/", "@", ".", "#"])
-    & admin_filter
-)
-async def tag_all_admins(_, message):
-
-    replied = message.reply_to_message
-    if len(message.command) < 2 and not replied:
-        await message.reply_text(
-            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hi Friends`"
-        )
-        return
-    if replied:
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in app.get_chat_admins(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
-                await replied.reply_text(usertxt)
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-    else:
-        text = message.text.split(None, 1)[1]
-
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in app.get_chat_admins(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
-                await app.send_message(
-                    message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
-                )
-                await asyncio.sleep(2)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-
-
-
-@app.on_message(
     filters.command(
         [
             "stopmention",
